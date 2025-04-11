a0183edea41243ea199577e59aa9e2ac07f30c6b
shahid <azamshahid052@gmail.com>
2025-04-11 17:14:16 +0500

    changes some ui

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,7 +6,7 @@ const Navigation = () => {
   return (
     <nav className='py-2'>
         <div className='container mx-auto  flex item-center justify-end'>
-            <div className='col1 w-[30%] flex gap-2 p-2 rounded-md cursor-pointer hover:bg-blue-300 bg-inline'>
+            <div className='col1 w-[30%] flex gap-2 p-2   cursor-pointer display:bg-inline '>
                 <CgMenuRightAlt className='mt-1 text-18px'/>
                 <button className='text-black  font-bold'>Shop By category</button>
                 <FaAngleDown className='text-18px mt-2 '/>
